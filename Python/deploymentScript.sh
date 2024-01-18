e842cc3aa5b124548fb02169f1fd2942849a45cd
Chuyuan Qu <chuyuanqu@microsoft.com>
2024-01-18 15:23:59 -0800

    add output

--- Python/deploymentScript.sh
+++ Python/deploymentScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Download and run the Python script
 curl -o script.py https://raw.githubusercontent.com/quchuyuan/OneClickDeployNewAssetType2Azure/main/Python/script.py
-python script.py
+python script.py > output.txt
+cat output.txt
