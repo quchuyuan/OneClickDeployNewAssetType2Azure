622ef97e3ceef7e23f6d46d4f2de9aa73a2db1d5
Chuyuan Qu <chuyuanqu@microsoft.com>
2024-01-18 16:09:03 -0800

    change to ps1 script

--- Python/deploymentScript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Download and run the Python script
-curl -o script.py https://raw.githubusercontent.com/quchuyuan/OneClickDeployNewAssetType2Azure/main/Python/script.py
-python script.py > output.txt
-cat output.txt
