c44e773ee8f5c32931f76a7656a206520ed97224
Chuyuan Qu <chuyuanqu@microsoft.com>
2024-01-22 13:14:49 -0800

    debug url

--- Simple/deploymentScript.sh
+++ Simple/deploymentScript.sh
@@ -24,7 +24,7 @@ accessToken=$(az account get-access-token --query accessToken -o tsv)
 
 # Define the Azure ML REST API endpoint for fetching component info
 apiUrl="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.MachineLearningServices/workspaces/$workspaceName/components/$componentName?api-version=2023-10-01"
-echo $apiUrl
+jq -n --arg apiUrl "$apiUrl" '{"apiUrl": $apiUrl}' > $AZ_SCRIPTS_OUTPUT_PATH
 
 # Use curl to make the HTTP GET request
 apiResponse=$(curl -X GET $apiUrl -H "Authorization: Bearer $accessToken" -H "Content-Type: application/json")
